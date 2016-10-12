2daceb23f4adfdac08839f0f959a2dd41c481e80
liuxiang <liuxiangaoai@163.com>
2016-10-12 21:46:44 +0800

    注释

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,9 +1,14 @@
+//利用cookie生成购物车的方法
 function goodsCart () {
+	//获取cookie
 	var listcookie = getCookie("goodscke");
 	var goodList = JSON.parse(listcookie);
 	//console.log(goodList);
+	//生成ul
 	var $ul = $("<ul />");
+	//单样商品的总价
 	var sum = 0;
+	//所有商品的总价
 	var sums = 0;
 	for (var i = 0; i < goodList.length; i++) {
 		var $li = $("<li />");
@@ -14,6 +19,7 @@ function goodsCart () {
 		sum = goodList[i].price*goodList[i].num;
 		//所有商品的总价
 		sums += sum;
+		//利用cookie生成购物车中商品列表结构
 		$("<input/>").attr({type:"checkbox",name:"chb",checked:"checked"}).appendTo($divdetail);
 		$("<a/>").append($("<img/>").attr({src:goodList[i].img}).addClass("good_img")).appendTo($divdetail);
 		$("<span/>").addClass("gname").html(goodList[i].entitle).appendTo($p);
@@ -32,35 +38,48 @@ function goodsCart () {
 		$("<a/>").addClass("plus").appendTo($pp);
 		$("<div/>").addClass("goods_qty").append($pp).appendTo($li);
 		$("<div/>").addClass("goods_sum").append($("<span/>").html("¥"+sum)).appendTo($li);
+		//把li添加到ul中
 		$li.appendTo($ul);
 	}
+	//把ul添加到购物车的div中
 	$ul.appendTo($(".cart_body"));
-	
+	//商品金额
 	var $listprice = $(".list_k .cart_price");
+	//会员优惠
 	var $hdyhprice = $(".hdyh_k .cart_price");
+	//支付金额
 	var $hyyhprice = $(".hyyh_k .cart_price");
 	$listprice.html(sums);
 	$hdyhprice.html("0");
 	$hyyhprice.html(sums);
 }
-
+//删除购物车里面的商品
 function clicks () {
+	//获取购物车中的商品列表
 	var $lis = $(".cart_body ul li");
+	//获取删除按钮
 	var $del = $lis.find(".cart_del");
-	var $reduce = $lis.find(".reduce");
-	var $plus = $lis.find(".plus");
+	//遍历删除按钮
 	$.each($del, function(idx,ele) {
+		//删除按钮的点击事件
 		$(this).on("click",function () {
+			//获取购物车的cookie
 			var listcookie = getCookie("goodscke");
+			//转换为数组
 			var goodList = JSON.parse(listcookie);
+			//移除当前点击的商品
 			$lis.eq(idx).remove();
+			//删除当前点击的商品的对象
 			goodList.splice(idx,1);
+			//把删除之后的数组转换为字符串
 			var goodscookie = JSON.stringify(goodList);
 			var d = new Date(10);
 			//把字符串存入cookie中
 			var goodscke = setCookie("goodscke",goodscookie,d,"/");
+			//显示新的购物车cookie中的商品件数
 			listcart();
-			goodsCart()
+			//调用生成购物车的方法
+			goodsCart();
 		});
 	});
 	
--- src/js/detail.js
+++ src/js/detail.js
@@ -1,20 +1,20 @@
 function listcart () {
 	var oldcke = getCookie("goodscke");
-	if (oldcke) {
+	if (oldcke) {//存在购物车cookie是
 		var good_list = JSON.parse(oldcke);
 		var nums = 0;
 		//循环遍历数组
 		for (var i = 0; i < good_list.length; i++) {
-			//数组中存在id与当前点击的商品id一样时改变当前下标中的num值
+			//获取购物车cookie中商品的总件数
 			nums += good_list[i].num;
 		}
 		var $cart = $(".cart a span");
+		//把总件数展示在购物车
 		$cart.html(nums);
 	}
-	
 }
 
-function details () {
+function details () {//详情页的方法
 	$.ajax({
 		url:"../assets/Json/lists.json",
 		dataType:"json",
@@ -25,6 +25,7 @@ function details () {
 				var listId = getCookie("listId");
 				//当id与json中的id相等时根据json生成详情页中的图片等
 				if (items.id == listId) {
+					//生成详情页商品的结构
 					$("<span />").html(items.title).appendTo($(".goodview_nav"));
 					$("<img/>").attr({src:items.imgurl}).appendTo($(".goodlist_img"));
 					$("<strong />").html(items.english_title).appendTo($(".goods_title"));
@@ -35,9 +36,11 @@ function details () {
 					$(".emoney strong").html(parseInt(items.price/100));
 					//放大镜插件
 					$(".goodlist_img").lzoom();
+					//商品数量加减
 					var $reduce = $(".reduce");
 					var $plus = $(".plus");
 					var $goodsnum = $(".goodsnum");
+					//商品数量减
 					$reduce.on("click",function () {
 						var i = parseInt($goodsnum.val());
 						if (i == "1") {
@@ -46,6 +49,7 @@ function details () {
 							$goodsnum.val(i-1);
 						}
 					});
+					//商品数量加
 					$plus.on("click",function () {
 						var i = parseInt($goodsnum.val());
 						$goodsnum.val(i+1);
@@ -55,14 +59,15 @@ function details () {
 					//购物车点击事件
 					//定义一个空数组
 					var goodList = [];
-					
+					//加入购物车点击事件
 					$cart_btn.on("click",function () {
 						//定义一个空对象
 						var goods = {};
 						//获取已有的cookie
 						var oldcookie = getCookie("goodscke");
 						//console.log(oldcookie);
-						if (oldcookie == "") {
+						//如果购物车cookie为空时
+						if (oldcookie == "") {//把要存进cookie中的数据存进空对象中
 							goods.id = items.id;
 							goods.img = items.imgurl;
 							goods.price = items.price;
@@ -71,13 +76,13 @@ function details () {
 							goods.color = $(".gcolor").val();
 							goods.size = $(".gsize").val();
 							goods.emoney = parseInt($(".emoney strong").html());
-							
+							//把对象存进数组中
 							goodList.push(goods);
 						} else{
 							//把cookie字符串转换为数组
 							goodList = JSON.parse(oldcookie);
-							//循环遍历数组
 							var j = true;
+							//循环遍历数组
 							for (var i = 0; i < goodList.length; i++) {
 								//数组中存在id与当前点击的商品id一样时改变当前下标中的num值
 								if (goodList[i].id == items.id) {
@@ -86,8 +91,7 @@ function details () {
 									break;
 								}
 							}
-							if (j) {
-								//goodList中id不相等时生成一个新的对象存入数组中
+							if (j) {//cookie中不存在一样的商品时添加一个新的对象进数组中
 								goods.id = items.id;
 								goods.img = items.imgurl;
 								goods.price = items.price;
@@ -107,21 +111,26 @@ function details () {
 						var goodscke = setCookie("goodscke",goodscookie,d,"/");
 						//加入购物车效果
 						var $img = $(".goodlist_img img");
+						//复制图片
 						var $copyimg = $img.clone();
 						var OFF = $img.offset();
 						var oWidth = $img.outerWidth();
-						
+						//给复制的图片添加样式
 						$copyimg.css({
 							position:"absolute",
 							left:OFF.left,
 							top:OFF.top,
 							width:oWidth
 						});
+						//把复制的图片添加到body中
 						$copyimg.appendTo("body");
 						var $cartOFF = $(".header .cart").offset();
 						var $cartOWidth = $(".header .cart").outerWidth();
+						//图片飞到购物车的运动
 						$copyimg.animate({top:$cartOFF.top,left:$cartOFF.left+$cartOWidth,width:0,opacity:0},2000,function () {
+							//图片飞到购物车时移除复制图片
 							$copyimg.remove();
+							//更新购物车商品总数
 							listcart();
 						});
 					});
